08a4beb5a60a7fc2305bd8854b32c55ad4a98ed8
Adrian White <snarkyboojum@gmail.com>
2020-02-22 21:56:02 +1100

    More hmac construction

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,67 @@ fn key_length_bits(key: &[u32]) -> usize {
     key_length * 32
 }
 
+// See https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.198-1.pdf
+// for HMAC construction algorithm and implementation details
+
+// TODO: this currently only works for SHA-512 where the block size is 1024 bits
+fn hmac_sha512(key: &[u32], text: &[u32]) -> [u32; 16] {
+    // determine K0
+    use byteorder::{BigEndian, ByteOrder};
+    use std::cmp::Ordering;
+    match key_length_bits(key).cmp(&1024) {
+        Ordering::Equal => {
+            // ipad
+            let mut ipad: [u8; 128] = [0u8; 128];
+            for elem in ipad.iter_mut() {
+                *elem = 0x36;
+            }
+            // opad
+            let mut opad: [u8; 128] = [0u8; 128];
+            for elem in opad.iter_mut() {
+                *elem = 0x5c;
+            }
+
+            let mut k0_ipad: [u8; 128] = [0u8; 128];
+            let mut k0_opad: [u8; 128] = [0u8; 128];
+            assert_eq!(key.len() * 4, ipad.len());
+            assert_eq!(key.len() * 4, opad.len());
+
+            for (i, item) in key.iter().enumerate() {
+                let mut quad = [0; 4];
+                BigEndian::write_u32(&mut quad, *item);
+
+                // k0 xor ipad
+                k0_ipad[i] = ipad[i] ^ quad[0];
+                k0_ipad[i + 1] = ipad[i + 1] ^ quad[1];
+                k0_ipad[i + 2] = ipad[i + 2] ^ quad[2];
+                k0_ipad[i + 3] = ipad[i + 3] ^ quad[3];
+
+                // k0 xor opad
+                k0_opad[i] = opad[i] ^ quad[0];
+                k0_opad[i + 1] = opad[i + 1] ^ quad[1];
+                k0_opad[i + 2] = opad[i + 2] ^ quad[2];
+                k0_opad[i + 3] = opad[i + 3] ^ quad[3];
+            }
+
+            // concat with text
+            //let k0_ipad_text = [&k0_ipad, text].concat();
+            //let hash = sha512_hash(&k0_ipad_text);
+        }
+
+        Ordering::Greater => {}
+        Ordering::Less => {}
+    }
+
+    [0u32; 16]
+}
+
 fn main() {
     println!("Welcome to the HMAC-SHA512 implementation!");
 
-    let text: &[u8] = &[
-        0x53, 0x61, 0x6D, 0x70, 0x6C, 0x65, 0x20, 0x6D, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x20,
-        0x66, 0x6F, 0x72, 0x20, 0x6B, 0x65, 0x79, 0x6C, 0x65, 0x6E, 0x3D, 0x62, 0x6C, 0x6F, 0x63,
-        0x6B, 0x6C, 0x65, 0x6E,
+    let text: &[u32] = &[
+        0x00005361, 0x6D706C65, 0x206D6573, 0x73616765, 0x20666F72, 0x206B6579, 0x6C656E3D,
+        0x626C6F63, 0x6B6C656E,
     ];
 
     let key: &[u32] = &[
@@ -29,14 +83,5 @@ fn main() {
         0x70717273, 0x74757677, 0x78797A7B, 0x7C7D7E7F,
     ];
 
-    // See https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.198-1.pdf
-    // for HMAC construction algorithm and implementation details
-
-    // determine K0
-    use std::cmp::Ordering;
-    match key_length_bits(key).cmp(&1024) {
-        Ordering::Equal => {}
-        Ordering::Greater => {}
-        Ordering::Less => {}
-    }
+    let mac = hmac_sha512(key, text);
 }
